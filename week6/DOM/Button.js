f8a8164c1dd3b29691503b965de37664e9a2d94d
do0hetfield <cjhjoonh77@icloud.com>
2026-04-28 18:16:53 +0900

    feat: 버튼 아이콘 추가

--- week6/DOM/Button.js
+++ week6/DOM/Button.js
@@ -4,6 +4,18 @@ class Button extends DOM {
     constructor(innerText, className) {
         super('button', innerText, className);
     }
+
+    addIcon(src, alt = '') {
+       const icon = new Image();   // Image() 객체 생성
+       icon.src = src;
+       icon.alt = alt;
+       icon.style.width = '16px';
+       icon.style.height = '16px';
+       icon.style.pointerEvents = 'none'; // 클릭 이벤트 방해 방지
+       this.node.innerHTML = '';          // 기존 텍스트 제거
+       this.node.appendChild(icon);
+       return this;                       // 메서드 체이닝 지원
+   }
 }
 
 export default Button;
--- week6/DOM/Complete.js
+++ week6/DOM/Complete.js
@@ -1,12 +1,14 @@
 import Button from "./Button.js";
 import Div from "./Div.js";
+const TRASH_ICON = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%23fa5252' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpolyline points='3 6 5 6 21 6'/%3E%3Cpath d='M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6'/%3E%3Cpath d='M10 11v6M14 11v6'/%3E%3C/svg%3E";
 
 class Complete {
     constructor(todo) {
         this.row = new Div('', 'row').node;
         this.textbox = new Div(todo, 'text-box');
         this.textbox.node.classList.add('done-text'); 
-        this.delBtn = new Button('삭제', 'del-btn');
+        this.delBtn = new Button(' ', 'del-btn');
+        this.delBtn.addIcon(TRASH_ICON, '삭제');
     }
 
     addRow() {
