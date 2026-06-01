e19efcb96b4959cd0354d9c1649bf2f1314b50fe
do0hetfield <cjhjoonh77@icloud.com>
2026-06-01 22:38:56 +0900

    feat: 아기사자 리스트 수정

--- week11/03-cra-lionlist/src/data/lionData.js
+++ week11/03-cra-lionlist/src/data/lionData.js
@@ -1,15 +1,36 @@
 export const lionData = [
-    // 👦 남성 멤버 5명
-    { id: 1, name: "류수민", gender: "male" },
-    { id: 2, name: "이준원", gender: "male" },
-    { id: 3, name: "최기훈", gender: "male" },
-    { id: 4, name: "최준혁", gender: "male" },
+    // 프론트엔드 10명
+    { id: 1, name: "류수민", part: "FE" },
+    { id: 2, name: "이준원", part: "FE" },
+    { id: 3, name: "최기훈", part: "FE" },
+    { id: 4, name: "최준혁", part: "FE" },
+    { id: 5, name: "김지수", part: "FE" },
+    { id: 6, name: "나희경", part: "FE" },
+    { id: 7, name: "소재희", part: "FE" },
+    { id: 8, name: "안서현", part: "FE" },
+    { id: 9, name: "오유민", part: "FE" },
+    { id: 10, name: "추서연", part: "FE" },
 
-    // 👧 여성 멤버 5명
-    { id: 5, name: "김지수", gender: "female" },
-    { id: 6, name: "나희경", gender: "female" },
-    { id: 7, name: "소재희", gender: "female" },
-    { id: 8, name: "안서현", gender: "female" },
-    { id: 9, name: "오유민", gender: "female" },
-    { id: 10, name: "추서연", gender: "female" },
+    // 기획디자인 9명
+    { id: 11, name: "곽효석", part: "PM/DE"},
+    { id: 12, name: "김기민", part: "PM/DE"},
+    { id: 13, name: "김다인", part: "PM/DE"},
+    { id: 14, name: "김성은", part: "PM/DE"},
+    { id: 15, name: "김지연", part: "PM/DE"},
+    { id: 16, name: "방세현", part: "PM/DE"},
+    { id: 17, name: "오채린", part: "PM/DE"},
+    { id: 18, name: "윤아영", part: "PM/DE"},
+    { id: 19, name: "후영리", part: "PM/DE"},
+
+    // 백엔드 10명
+    { id: 20, name: "권민찬", part: "BE"},
+    { id: 21, name: "김예은", part: "BE"},
+    { id: 22, name: "김주연", part: "BE"},
+    { id: 23, name: "김지윤", part: "BE"},
+    { id: 24, name: "김희선", part: "BE"},
+    { id: 25, name: "서유진", part: "BE"},
+    { id: 26, name: "안지요", part: "BE"},
+    { id: 27, name: "이시형", part: "BE"},
+    { id: 28, name: "조상원", part: "BE"},
+    { id: 29, name: "차예은", part: "BE"},
 ];
