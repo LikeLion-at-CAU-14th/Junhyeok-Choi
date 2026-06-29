df14950946896cae47a10a784a0fecc14367ba6f
do0hetfield <cjhjoonh77@icloud.com>
2026-06-29 17:46:57 +0900

    feat: 퀴즈 UI 및 답안 선택 기능

--- week12/src/pages/Quiz.jsx
+++ week12/src/pages/Quiz.jsx
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Quiz = () => {
-  return (
-    <div>Quiz Page</div>
-  )
-}
+  const [questions, setQuestions] = useState([]);
+  const [userAnswers, setUserAnswers] = useState([]);
 
-export default Quiz
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      const response = await axios.get('https://week12-api-rcwo.onrender.com/api/questions');
+      setQuestions(response.data);
+    };
+    fetchQuestions();
+  }, []);
 
-const [questions, setQuestions] = useState([]);
-
-useEffect(() => {
-  const fetchQuestions = async () => {
-    const response = await axios.get('https://week12-api-rcwo.onrender.com/api/questions');
-    setQuestions(response.data);
+  const handleSelect = (questionId, answer) => {
+    setUserAnswers(prev => {
+      const filtered = prev.filter(a => a.id !== questionId);
+      return [...filtered, { id: questionId, answer }];
+    });
   };
-  fetchQuestions();
-}, []);
+
+  return (
+    <div>
+      {questions.map((q) => (
+        <div key={q.id}>
+          <p>{q.question}</p>
+          {q.answers.map((answer) => (
+            <button
+              key={answer}
+              onClick={() => handleSelect(q.id, answer)}
+              style={{
+                backgroundColor: userAnswers.find(a => a.id === q.id)?.answer === answer
+                  ? '#75b5f5'
+                  : '',
+              }}
+            >
+              {answer}
+            </button>
+          ))}
+        </div>
+      ))}
+      <button
+        disabled={userAnswers.length < questions.length}
+        onClick={() => alert('제출!')}
+      >
+        제출하기
+      </button>
+    </div>
+  );
+};
+
+export default Quiz;
