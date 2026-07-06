d9619699d0da9c685637f02ab679f89bec4ee672
do0hetfield <cjhjoonh77@icloud.com>
2026-07-06 20:49:02 +0900

    feat: App에 UserInfoProvider 연결

--- week13/src/App.jsx
+++ week13/src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import Layout from './components/layout/Layout'
 import Home from './pages/Home'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import MyPage from './pages/MyPage'
+import { UserInfoProvider } from './context/UserInfoContext'
 
 const App = () => {
   return (
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/mypage" element={<MyPage />} />
-        </Routes>
-      </Layout>
+      <UserInfoProvider>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mypage" element={<MyPage />} />
+          </Routes>
+        </Layout>
+      </UserInfoProvider>
   )
 }
 export default App
