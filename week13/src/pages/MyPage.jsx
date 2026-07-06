395a2385956284d648a5712409c9f97034baa822
do0hetfield <cjhjoonh77@icloud.com>
2026-07-06 20:58:35 +0900

    feat: MyPage에서 전역 상태 조회 및 렌더링

--- week13/src/pages/MyPage.jsx
+++ week13/src/pages/MyPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Card, Title, Wrapper } from '../components/layout/common';
+import { useUserInfo } from '../context/UserInfoContext';
 
 const MyPage = () => {
+  const { state } = useUserInfo();
+
   return (
-    <div>MyPage</div>
+    <Wrapper>
+      <Card>
+        <Title>내 정보</Title>
+        <p>이름: {state.name}</p>
+        <p>이메일: {state.email}</p>
+        <p>생년월일: {state.birth}</p>
+        <p>성별: {state.gender}</p>
+      </Card>
+    </Wrapper>
   )
 }
 
